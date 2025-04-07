c67314663069d5a5095f05d533be2773ea9f15ab
Jon-Tom Roscher <116283136+BegeistertsLab@users.noreply.github.com>
2025-04-07 15:07:31 +0200

    Initial commit

---
+++ src/main/java/net/begeistert/sodaverse/BegeistertsSodaverseClient.java
@@ -0,0 +1,10 @@
+package net.begeistert.sodaverse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BegeistertsSodaverseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/begeistert/sodaverse/BegeistertsSodaverseDataGenerator.java
@@ -0,0 +1,11 @@
+package net.begeistert.sodaverse;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BegeistertsSodaverseDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
